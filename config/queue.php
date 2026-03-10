51257364e4f5d376d1e6bc45ccd3938e3c5fb91f
mustakrakishe <mustakrkish@gmail.com>
2026-03-10 14:29:17 +0200

    Add rabbitmq connection to queue config

--- config/queue.php
+++ config/queue.php
@@ -89,6 +89,18 @@ return [
             ],
         ],
 
+        'rabbitmq' => [
+            'driver' => 'rabbitmq',
+            'hosts' => [
+                [
+                    'host' => env('RABBITMQ_HOST', '127.0.0.1'),
+                    'port' => env('RABBITMQ_PORT', 5672),
+                    'user' => env('RABBITMQ_USER', 'guest'),
+                    'password' => env('RABBITMQ_PASSWORD', 'guest'),
+                    'vhost' => env('RABBITMQ_VHOST', '/'),
+                ],
+            ],
+        ],
     ],
 
     /*
